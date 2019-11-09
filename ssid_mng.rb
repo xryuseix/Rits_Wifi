4f75b5414412a39e27897a88fe8be335877d42e9
AtLab-jp <abe12@mccc.jp>
2019-11-09 17:55:00 +0900

    管理modeの選択

--- ssid_mng.rb
+++ ssid_mng.rb
@@ -1,14 +1,25 @@
 require 'yaml'
 
-
 data = YAML.load(open('./config/login.yml', 'r'))
-p data
 
-data.keys.each_with_index do |ssid, i|
-  puts "#{i+1}: #{ssid}"
+# ===== Select Mode ==========
+puts " | 1: Change SSID config"
+puts " | 2: Add    SSID config"
+print "Select Mode: "
+mode = gets.to_i
+# =====
+
+# ===== Select SSID ==========
+puts "\n"
+data.keys.each_with_index do  |ssid, i|
+  puts " | #{i+1}: #{ssid}"
 end
 
 print "Select SSID Number: "
 ssid = data.values[gets.to_i-1]
+# =====
+
+
+p mode
 p ssid
 
