5f1e71fee60df32db59444f196deeb9ad4fae1a5
AtLab-jp <abe12@mccc.jp>
2019-11-09 11:53:22 +0900

    yamlがない場合は，新規作成

--- submit.py
+++ submit.py
@@ -1,12 +1,26 @@
 from crawling import Crawling
 import yaml
+import os
 
 ## ------*----- config -----*----- ##
 # Webauth URL
 url = "https://webauth.ritsumei.ac.jp/fs/customwebauth/login.html"
 # RainbowID
-account = open("./config/rainbow.yml", "r+")
-account = yaml.load(account)
+if os.path.exists("./config/rainbow.yml"):
+    print('hello')
+    account = open("./config/rainbow.yml", "r+")
+    account = yaml.load(account)
+else:
+    print('file not exist')
+    os.makedirs('./config/', exist_ok=True)
+    with open('./config/rainbow.yml', 'w') as f:
+        yaml.dump({
+            "ID": input('your rainbow ID:'),
+            "PW": input('your rainbow PW:')
+        }, f, default_flow_style=False)
+
+
+exit()
 
 agent = Crawling(url)
 # フォーム送信
