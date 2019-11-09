dfe69dffc528dedcd94e3dfe35bac296eee96cea
AtLab-jp <abe12@mccc.jp>
2019-11-09 17:45:20 +0900

    rakeeタスクを解除

--- ssid_mng.rb
+++ ssid_mng.rb
@@ -1,5 +1,6 @@
 require 'yaml'
 
+
 data = YAML.load(open('./config/login.yml', 'r'))
 p data
 
@@ -7,7 +8,7 @@ data.keys.each_with_index do |ssid, i|
   puts "#{i+1}: #{ssid}"
 end
 
-
 print "Select SSID Number: "
 ssid = data.values[gets.to_i-1]
 p ssid
+
