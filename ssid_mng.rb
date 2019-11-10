48b5924ac4a6f6ce88bbdf1ac37a35888e54235f
xryuseix <ryusei143.shootingstar@gmail.com>
2019-11-10 10:41:20 +0900

    ssid_mngのSelectSSID項目追加

--- ssid_mng.rb
+++ ssid_mng.rb
@@ -5,21 +5,41 @@ data = YAML.load(open('./config/login.yml', 'r'))
 # ===== Select Mode ==========
 puts " | 1: Change SSID config"
 puts " | 2: Add    SSID config"
+puts " | 3: Exit"
 print "Select Mode: "
 mode = gets.to_i
 # =====
 
-# ===== Select SSID ==========
-puts "\n"
-data.keys.each_with_index do  |ssid, i|
-  puts " | #{i+1}: #{ssid}"
+case mode
+when 1 then
+  ## -----*----- Select SSID -----*----- ##
+  puts "\n"
+  data.keys.each_with_index do  |ssid, i|
+    puts " | #{i+1}: #{ssid}"
+  end
+  print "Select SSID Number: "
+  datanum = gets.to_i-1
+  account = data.values[datanum]
+
+  puts "\n"
+  account.keys.each_with_index do  |value, i|
+    puts " | #{i+1}: #{value}"
+  end
+  print "Select Change Item Number: "
+  changenum = gets.to_i-1
+
+
+  print "Imput the value \"#{account.values[changenum]}\" ->: "
+  data[data.keys[datanum]][account.keys[changenum]] = gets.chomp
+
+when 2 then
+  ## -----*----- Add SSID -----*----- ##
+
+
+when 3 then
+  ## -----*----- Exit -----*----- ##
+  exit
 end
 
-print "Select SSID Number: "
-ssid = data.values[gets.to_i-1]
-# =====
-
-
-p mode
-p ssid
-
+file = './config/login.yml'
+YAML.dump(data, File.open(file, 'w'))
