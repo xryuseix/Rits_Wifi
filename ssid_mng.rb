1394fb9a5eabe6699f57cc6ece7d9e3d6b50b294
AtLab-jp <abe12@mccc.jp>
2019-11-10 22:51:23 +0900

    コメントの修正

--- ssid_mng.rb
+++ ssid_mng.rb
@@ -7,7 +7,7 @@ puts " | 1: Change SSID config"
 puts " | 2: Add    SSID config"
 print "Select Mode: "
 mode = gets.to_i
-# =====
+# ===============
 
 # ===== Select SSID ==========
 puts "\n"
@@ -17,7 +17,27 @@ end
 
 print "Select SSID Number: "
 ssid = data.values[gets.to_i-1]
-# =====
+# ===============
+
+
+data = YAML.load(open('./config/login.yml', 'r'))
+
+# ===== Select Mode ==========
+puts " | 1: Change SSID config"
+puts " | 2: Add    SSID config"
+print "Select Mode: "
+mode = gets.to_i
+# ===============
+
+# ===== Select SSID ==========
+puts "\n"
+data.keys.each_with_index do  |ssid, i|
+puts " | #{i+1}: #{ssid}"
+end
+
+print "Select SSID Number: "
+ssid = data.values[gets.to_i-1]
+# ===============
 
 
 p mode
