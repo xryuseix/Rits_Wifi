d5af1a25c1267b2daf406b482ba23d8e17fd385d
xryuseix <ryusei143.shootingstar@gmail.com>
2019-11-11 21:44:58 +0900

    ssid_mng.rbが動くように変更

--- ssid_mng.rb
+++ ssid_mng.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
 def check_ssid
-  ## -----*----- 現在接続中のSSID検出 -----*----- ##
+  # ===== 現在接続中のSSID検出 ==========
   ret = `/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | /usr/bin/grep -ie '^\s*ssid'  | cut -d ":" -f 2`
   ret.strip!
   return ret
@@ -19,7 +19,7 @@ mode = gets.to_i
 
 case mode
 when 1 then
-  ## -----*----- Select SSID -----*----- ##
+  # ===== Select SSID ==========
   puts "\n"
   data.keys.each_with_index do  |ssid, i|
     puts " | #{i+1}: #{ssid}"
@@ -28,45 +28,19 @@ when 1 then
   datanum = gets.to_i-1
   account = data.values[datanum]
 
-<<<<<<< HEAD
-print "Select SSID Number: "
-ssid = data.values[gets.to_i-1]
-# ===============
-
-
-data = YAML.load(open('./config/login.yml', 'r'))
-
-# ===== Select Mode ==========
-puts " | 1: Change SSID config"
-puts " | 2: Add    SSID config"
-print "Select Mode: "
-mode = gets.to_i
-# ===============
-
-# ===== Select SSID ==========
-puts "\n"
-data.keys.each_with_index do  |ssid, i|
-puts " | #{i+1}: #{ssid}"
-end
-
-print "Select SSID Number: "
-ssid = data.values[gets.to_i-1]
-# ===============
-=======
   puts "\n"
   account.keys.each_with_index do  |value, i|
     puts " | #{i+1}: #{value}"
   end
   print "Select Change Item Number: "
   changenum = gets.to_i-1
->>>>>>> 6f36021046180ab5260caa2958ec6630d29f04a4
-
 
   print "Imput the value \"#{account.values[changenum]}\" ->: "
   data[data.keys[datanum]][account.keys[changenum]] = gets.chomp
+# ===============
 
 when 2 then
-  ## -----*----- Add SSID -----*----- ##
+  # ===== Add SSID ==========
   print "Input your ID: "; id = gets.chop
   print "Input your PW: "; pw = gets.chop
   print "Input URL: "; url = gets.chop
@@ -75,7 +49,7 @@ when 2 then
   data[ssid] = {'ID' => id, 'PW' => pw, 'URL' => url}
 
 when 3 then
-  ## -----*----- Exit -----*----- ##
+  # ===== Exit ==========
   exit
 end
 
